da3a49cffe46884e57667883f1df4ad2a0583be2
Liyou Chen <LC1239@live.mdx.ac.uk>
2023-11-10 00:34:49 +0800

    update

---
+++ data.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <strong><em>irresistible</em></strong>, organic goodness for your cat.",
+        price: 2000,
+        image: "Images/cat.png",
+        availableInventory: 5
+    },
+    {
+        id: 1002,
+        title: "Cat Food, 26lb bag",
+        description: "A 25 pound bag of <strong><em>irresistible</em></strong>, organic goodness for your cat.",
+        price: 200,
+        image: "Imgs/cat.png",
+        availableInventory: 5
+    },
+    {
+        id: 1003,
+        title: "Cat Food, 27lb bag",
+        description: "A 25 pound bag of <strong><em>irresistible</em></strong>, organic goodness for your cat.",
+        price: 20,
+        image: "Imgs/cat.png",
+        availableInventory: 5
+    },
+  ];
+  
